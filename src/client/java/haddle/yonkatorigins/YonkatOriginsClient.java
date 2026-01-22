1dfd78d1f527d57d867d2ef3d54955237ac03760
Haddle <94085928+hadll@users.noreply.github.com>
2026-01-23 03:15:31 +1100

    init commit

---
+++ src/client/java/haddle/yonkatorigins/YonkatOriginsClient.java
@@ -0,0 +1,10 @@
+package haddle.yonkatorigins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YonkatOriginsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
