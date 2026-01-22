2b60125ab3b11cece4636f8b59b0bc2b2a4944dd
Haddle <94085928+hadll@users.noreply.github.com>
2026-01-23 08:14:43 +1100

    changed amount to value and added has_fish

--- src/main/java/haddle/yonkatorigins/registry/YOActionTypes.java
+++ src/main/java/haddle/yonkatorigins/registry/YOActionTypes.java
@@ -3,6 +3,7 @@ package haddle.yonkatorigins.registry;
 import haddle.yonkatorigins.YonkatOrigins;
 import io.github.apace100.apoli.Apoli;
 import io.github.apace100.apoli.power.factory.action.ActionFactory;
+import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
 import io.github.apace100.apoli.registry.ApoliRegistries;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
@@ -15,10 +16,18 @@ public class YOActionTypes {
         Registry.register(ApoliRegistries.ENTITY_ACTION, actionFactory.getSerializerId(), actionFactory);
     }
 
+    private static void register(ConditionFactory<Entity> conditionFactory) {
+        Registry.register(ApoliRegistries.ENTITY_CONDITION, conditionFactory.getSerializerId(), conditionFactory);
+    }
+
     public static void init(){
-        register(new ActionFactory<>(Apoli.identifier("add_fish"), new SerializableData().add("amount", SerializableDataTypes.DOUBLE),
+        register(new ActionFactory<>(Apoli.identifier("add_fish"), new SerializableData().add("value", SerializableDataTypes.DOUBLE),
             (data, entity) -> {
                 YOComponents.FISH.get(entity).changeFish(data.getDouble("amount"));
             }));
+        register(new ConditionFactory<>(Apoli.identifier("has_fish"), new SerializableData().add("value",  SerializableDataTypes.DOUBLE),
+                (data, entity) -> {
+                    return (YOComponents.FISH.get(entity).getFish() >= (double) data.get("amount"));
+                }));
     }
 }
